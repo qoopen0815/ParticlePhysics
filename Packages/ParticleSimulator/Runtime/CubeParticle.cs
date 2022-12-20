4abe956a267ae61e7fb55e872522d09f65b1716d
TomohiroKuwano <ku_engineer_wa@yahoo.com>
2022-12-21 00:28:05 +0900

    Modification of package structure (#2)
    パッケージ開発の準備が整った

---
+++ Packages/ParticleSimulator/Runtime/CubeParticle.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+
+namespace GpuDeformableTerrain
+{
+    public class CubeParticle : GranularSubstance
+    {
+        private static uint _granularParticleNum = 9;
+        public static float particleRatio = 0.3f;
+
+        #region Accessor
+        public uint GranularParticleNum => _granularParticleNum;
+        public float TotalMass => totalMass;
+        public Vector3 CenterOfMass => centerOfMass;
+        public float3x3 InertialMoment => inertialMoment;
+        public ParticleSubstance[] Particles => particles;
+        #endregion
+
+        public CubeParticle(float radius = 0.04f, float density = 2000.0f) : base(_granularParticleNum)
+        {
+            particles = InitializeParticle(radius, density);
+            totalMass = CalculateTotalParticleMass(particles, density);
+            centerOfMass = CalculateCenterOfMass(particles);
+            inertialMoment = CalculateInverseInertialMoment(particles);
+        }
+
+        protected override ParticleSubstance[] InitializeParticle(float particleRadius, float particleDensity)
+        {
+            float CORNER = 1.0f / math.sqrt(3.0f);
+            ParticleSubstance[] p = new ParticleSubstance[_granularParticleNum];
+            p[0] = new ParticleSubstance(particleRadius, CalculateParticleMass(particleRadius, particleDensity), new float3(0.0f, 0.0f, 0.0f) * particleRadius);
+            p[1] = new ParticleSubstance(particleRadius * particleRatio, CalculateParticleMass(particleRadius * particleRatio, particleDensity), new float3(CORNER, CORNER, CORNER) * particleRadius * (1.0f + particleRatio));
+            p[2] = new ParticleSubstance(particleRadius * particleRatio, CalculateParticleMass(particleRadius * particleRatio, particleDensity), new float3(CORNER, CORNER, -CORNER) * particleRadius * (1.0f + particleRatio));
+            p[3] = new ParticleSubstance(particleRadius * particleRatio, CalculateParticleMass(particleRadius * particleRatio, particleDensity), new float3(-CORNER, CORNER, -CORNER) * particleRadius * (1.0f + particleRatio));
+            p[4] = new ParticleSubstance(particleRadius * particleRatio, CalculateParticleMass(particleRadius * particleRatio, particleDensity), new float3(-CORNER, CORNER, CORNER) * particleRadius * (1.0f + particleRatio));
+            p[5] = new ParticleSubstance(particleRadius * particleRatio, CalculateParticleMass(particleRadius * particleRatio, particleDensity), new float3(CORNER, -CORNER, CORNER) * particleRadius * (1.0f + particleRatio));
+            p[6] = new ParticleSubstance(particleRadius * particleRatio, CalculateParticleMass(particleRadius * particleRatio, particleDensity), new float3(CORNER, -CORNER, -CORNER) * particleRadius * (1.0f + particleRatio));
+            p[7] = new ParticleSubstance(particleRadius * particleRatio, CalculateParticleMass(particleRadius * particleRatio, particleDensity), new float3(-CORNER, -CORNER, -CORNER) * particleRadius * (1.0f + particleRatio));
+            p[8] = new ParticleSubstance(particleRadius * particleRatio, CalculateParticleMass(particleRadius * particleRatio, particleDensity), new float3(-CORNER, -CORNER, CORNER) * particleRadius * (1.0f + particleRatio));
+            return p;
+        }
+    }
+}
---
+++ Packages/ParticleSimulator/Runtime/GranularSubstance.cs
@@ -0,0 +1,79 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace GpuDeformableTerrain
+{
+    public struct ParticleSubstance
+    {
+        public float radius;
+        public float mass;
+        public Vector3 offsetFromGranularCenter;
+
+        public ParticleSubstance(float radius, float mass, Vector3 offsetFromGranularCenter)
+        {
+            this.radius = radius;
+            this.mass = mass;
+            this.offsetFromGranularCenter = offsetFromGranularCenter;
+        }
+    }
+
+    public abstract class GranularSubstance
+    {
+        protected ParticleSubstance[] particles;
+        protected float totalMass;
+        protected float3 centerOfMass;
+        protected float3x3 inertialMoment;
+
+        protected GranularSubstance(uint particleNum)
+        {
+            this.particles = new ParticleSubstance[particleNum];
+        }
+
+        protected float CalculateParticleMass(float particleRadius, float particleDensity)
+        {
+            float particle_volume = (4.0f / 3.0f) * math.PI * math.pow(particleRadius, 3);
+            float particle_mass = particle_volume * particleDensity;
+            return particle_mass;
+        }
+
+        protected float3x3 CalculateInverseInertialMoment(ParticleSubstance[] componentParticles)
+        {
+            float3x3 inertialMoment = float3x3.zero;
+
+            foreach (ParticleSubstance particle in componentParticles)
+            {
+                float3x3 particleInertialMoment = float3x3.identity * (2.0f / 5.0f) * particle.mass * math.pow(particle.radius, 2);
+
+                float3 offset = particle.offsetFromGranularCenter;
+                float offsetSquared = math.dot(offset, offset);
+                float3x3 offsetParticleInertialMoment = particleInertialMoment + particle.mass * (offsetSquared * float3x3.identity - math.mul(offset, offset));
+                inertialMoment += offsetParticleInertialMoment;
+            }
+            inertialMoment = math.transpose(inertialMoment);
+
+            return inertialMoment;
+        }
+
+        protected float CalculateTotalParticleMass(ParticleSubstance[] componentParticles, float particleDensity)
+        {
+            float totalParticleMass = 0;
+            foreach (ParticleSubstance particle in componentParticles)
+            {
+                totalParticleMass += CalculateParticleMass(particle.radius, particleDensity);
+            }
+            return totalParticleMass;
+        }
+
+        protected float3 CalculateCenterOfMass(ParticleSubstance[] componentParticles)
+        {
+            float3 centerOfMass = float3.zero;
+            foreach (ParticleSubstance particle in componentParticles)
+            {
+                centerOfMass += new float3(particle.offsetFromGranularCenter);
+            }
+            return centerOfMass / (float)this.particles.Length;
+        }
+
+        protected abstract ParticleSubstance[] InitializeParticle(float particleRadius, float particleDensity);
+    }
+}
