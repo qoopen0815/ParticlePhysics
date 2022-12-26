f772c03170c903cfea68af4372e1320b1ca85b3d
TomohiroKuwano <ku_engineer_wa@yahoo.com>
2022-12-26 22:00:29 +0900

    Update SandSimulator.cs

--- Packages/ParticleSimulator/Runtime/Scripts/Simulation/SandSimulator.cs
+++ Packages/ParticleSimulator/Runtime/Scripts/Simulation/SandSimulator.cs
@@ -41,6 +41,9 @@ namespace ParticleSimulator
         // Terrain Data
         [SerializeField] private Terrain _terrain;
 
+        // Object Data
+        [SerializeField] private Mesh _mesh;
+
         // Simulation
         [SerializeField] private float _maxAllowableTimestep = 0.0005f;     // 最大時間刻み幅
         [SerializeField] private Vector3 _gravity = Physics.gravity;        // 重力
@@ -49,11 +52,11 @@ namespace ParticleSimulator
         // ComputeShader
         private ComputeShader _solver;
         private ParticleBuffer _particleBuffer;
-        private ParticleBuffer _terrainParticleBuffer;
         private ParticleBuffer _objectParticleBuffer;
+        private GraphicsBuffer _terrainBuffer;
         private GraphicsBuffer _particleCollisionForce;
-        private GraphicsBuffer _terrainCollisionForce;
         private GraphicsBuffer _objectCollisionForce;
+        private GraphicsBuffer _terrainCollisionForce;
 
         // GPU
         private int _threadSize = 32;     // コンピュートシェーダ側のスレッド数
@@ -88,8 +91,8 @@ namespace ParticleSimulator
         private void OnDestroy()
         {
             _particleBuffer.Release();
-            _terrainParticleBuffer.Release();
             _objectParticleBuffer.Release();
+            _terrainBuffer.Release();
             _particleCollisionForce.Release();
             _terrainCollisionForce.Release();
             _objectCollisionForce.Release();
@@ -98,21 +101,23 @@ namespace ParticleSimulator
 
         private void InitBuffer()
         {
-            // Particle
-            var p = ParticleData.GenerateSphere((int)_particleNum, new Vector3(0, 0, 0), 30);
+            // Init Particle Buffer
+            var p = ParticleType.GenerateSphere((int)_particleNum, new Vector3(0, 0, 0), 30);
             var ps = new Substance.TetrahedronParticle();
             _particleBuffer = new ParticleBuffer(p, ps.Elements);
 
-            // Terrain
-            var t = ParticleData.GenerateFromTerrain((int)_particleNum, _terrain);
-            var ts = new Substance.TetrahedronParticle();
-            _terrainParticleBuffer = new ParticleBuffer(t, ts.Elements);
+            // Init Object Particle Buffer
+            var o = ParticleType.GenerateFromMesh((int)_particleNum, _mesh);
+            var os = new Substance.SimpleParticle();
+            _objectParticleBuffer = new ParticleBuffer(o, os.Elements);
 
-            // Object
-            var o = ParticleData.GenerateFromMesh((int)_particleNum);
-            var os = new Substance.TetrahedronParticle();
-            _terrainParticleBuffer = new ParticleBuffer(o, os.Elements);
+            // Init Terrain Bufer
+            _terrainBuffer = new GraphicsBuffer(
+                GraphicsBuffer.Target.Structured,
+                (int)_particleNum,
+                Marshal.SizeOf(typeof(TerrainType)));
 
+            // Init Force Bufer
             _particleCollisionForce = new GraphicsBuffer(
                 GraphicsBuffer.Target.Structured,
                 (int)_particleNum,
@@ -130,7 +135,7 @@ namespace ParticleSimulator
         public void UpdateParticle(ref GraphicsBuffer particles)
         {
             CalculateParticleCollisionForce(ref _particleBuffer);
-            CalculateGroundCollision(ref _particleBuffer, _terrainParticleBuffer);
+            CalculateTerrainCollision(ref _particleBuffer, _terrainBuffer);
             CalculateObjectCollision(ref _particleBuffer, _objectParticleBuffer);
         }
 
@@ -150,7 +155,7 @@ namespace ParticleSimulator
 
         }
 
-        private void CalculateGroundCollision(ref ParticleBuffer particleBuffer, ParticleBuffer groundBuffer)
+        private void CalculateTerrainCollision(ref ParticleBuffer particleBuffer, GraphicsBuffer terrainBuffer)
         {
 
         }
