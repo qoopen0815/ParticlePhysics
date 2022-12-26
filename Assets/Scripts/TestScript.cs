5ec8080dc925111a21e3d91110ab36940b3eba25
TomohiroKuwano <ku_engineer_wa@yahoo.com>
2022-12-26 21:25:30 +0900

    Rename NewBehaviourScript.cs -> TestScript.cs

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TestScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
