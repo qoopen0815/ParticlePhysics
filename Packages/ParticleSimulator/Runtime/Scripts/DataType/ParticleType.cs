d5ac36d30cac712267a7557a0d4deda8498d2b1c
TomohiroKuwano <ku_engineer_wa@yahoo.com>
2022-12-27 22:58:55 +0900

    Update ParticleType.cs

--- Packages/ParticleSimulator/Runtime/Scripts/DataType/ParticleType.cs
+++ Packages/ParticleSimulator/Runtime/Scripts/DataType/ParticleType.cs
@@ -8,17 +8,18 @@ namespace ParticleSimulator
     {
         public Vector3 position;
         public Vector3 velocity;
-        public Quaternion orientation;
+        public Vector4 orientation;
         public Vector3 angularVelocity;
 
         public static ParticleType[] GeneratePoint(int particleNum, Vector3 centerPos)
         {
             var particles = new ParticleType[particleNum];
+            var identityOrientation = Quaternion.identity;
             for (int i = 0; i < particleNum; i++)
             {
                 particles[i].position = centerPos;
                 particles[i].velocity = Vector3.zero;
-                particles[i].orientation = Quaternion.identity;
+                particles[i].orientation = new Vector4(identityOrientation.x, identityOrientation.y, identityOrientation.z, identityOrientation.w);
                 particles[i].angularVelocity = Vector3.zero;
             }
             return particles;
@@ -27,11 +28,12 @@ namespace ParticleSimulator
         public static ParticleType[] GenerateSphere(int particleNum, Vector3 centerPos, float radius)
         {
             var particles = new ParticleType[particleNum];
+            var identityOrientation = Quaternion.identity;
             for (int i = 0; i < particleNum; i++)
             {
                 particles[i].position = centerPos + Random.insideUnitSphere * radius;   // 球形に粒子を初期化する
                 particles[i].velocity = Vector3.zero;
-                particles[i].orientation = Quaternion.identity;
+                particles[i].orientation = new Vector4(identityOrientation.x, identityOrientation.y, identityOrientation.z, identityOrientation.w);
                 particles[i].angularVelocity = Vector3.zero;
             }
             return particles;
