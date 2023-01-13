607dac876b5b7939551d4e06fd959d49c77a25af
TomohiroKuwano <ku_engineer_wa@yahoo.com>
2023-01-13 17:27:16 +0900

    refactor: rename classes and methods (#16) (#17)

--- Packages/ParticlePhysics/Runtime/Scripts/Particle/State.cs
+++ Packages/ParticlePhysics/Runtime/Scripts/Particle/State.cs
@@ -4,16 +4,16 @@ using UnityEngine.VFX;
 namespace ParticlePhysics.Particle
 {
     [VFXType(VFXTypeAttribute.Usage.GraphicsBuffer)]
-    public struct ParticleStatus
+    public struct State
     {
         public Vector3 position;
         public Vector3 velocity;
         public Vector4 orientation;
         public Vector3 angularVelocity;
 
-        public static ParticleStatus[] GeneratePoint(int particleNum, Vector3 centerPos)
+        public static State[] GeneratePoint(int particleNum, Vector3 centerPos)
         {
-            var particles = new ParticleStatus[particleNum];
+            var particles = new State[particleNum];
             var identityOrientation = Quaternion.identity;
             for (int i = 0; i < particleNum; i++)
             {
@@ -25,9 +25,9 @@ namespace ParticlePhysics.Particle
             return particles;
         }
 
-        public static ParticleStatus[] GenerateSphere(int particleNum, Vector3 centerPos, float radius)
+        public static State[] GenerateSphere(int particleNum, Vector3 centerPos, float radius)
         {
-            var particles = new ParticleStatus[particleNum];
+            var particles = new State[particleNum];
             var identityOrientation = Quaternion.identity;
             for (int i = 0; i < particleNum; i++)
             {
@@ -39,9 +39,9 @@ namespace ParticlePhysics.Particle
             return particles;
         }
     
-        public static ParticleStatus[] GenerateFromMesh(int particleNum, Mesh mesh)
+        public static State[] GenerateFromMesh(int particleNum, Mesh mesh)
         {
-            var particles = new ParticleStatus[particleNum];
+            var particles = new State[particleNum];
             return particles;
         }
     };
